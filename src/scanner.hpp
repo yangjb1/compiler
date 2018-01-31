37391275f0bcfbbd77c2572381658e784f79208d
yangjb1 <yangjb@mail.uc.edu>
2018-01-31 08:03:46 -0500

    add identifier check

--- src/scanner.hpp
+++ src/scanner.hpp
@@ -642,19 +642,26 @@ static int ScanOneToken( FILE *fp, token_t *token) {
             token->type = T_UNKNOWN;
             return T_UNKNOWN;
 
-
-
-
-
-
-        /*    
         case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I':
         case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case "R':
         case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
         case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i':
         case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r':
         case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
-        */
+            for (;;) {
+                ch = getch(fp);
+                if (isdigit(ch) || isalph(ch) || ch == '_') {
+                    token->val.stringVal += ch;
+                    continue;
+                }
+
+                break;
+            }
+            token->type = T_IDENTIFIER;
+            return T_IDENTIFIER;
+
+
+
        case '"':
             token->type = T_STRING;
             token->val.stringVal[0] = ch;
